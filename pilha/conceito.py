96e49f0cd4f18a801e3582d70a0359a303a1e512
jonlucaz <jonathanlucastri@gmail.com>
2022-11-04 08:31:18 -0300

    introdução de uma estrutura pilha em python

---
+++ pilha/conceito.py
@@ -0,0 +1,13 @@
+# Conceitos de estrutura de dados
+# Estremidades em estrutura de dados podem ser chamadas de topo e base
+
+# O que é uma pilha (stack)
+# Coleção ordenada de itens
+# FILO - First in, Last Out
+# Comandos para pilha em python:
+# Stack() cria uma nova pilha vazia. Não necessita parâmetros e retorna uma pilha vazia.
+# push(item) insere um novo item na pilha. A operação necessita o item e não retorna coisa alguma.
+# pop() remove o item que está no topo da pilha. Não necessita parametros e retorna o item removido.
+# peek() retorna o item no topo da pilha mas não o remove da pilha. Não necessita de parâmetros. Não é modificada.
+# isEmpty() testa se a pilha está vazia. Não necessita parâmetros e retorna um valor booleano.
+# size() retorna o número de item na pilha. Não necessita parâmetros e retorna um inteiro; valor do tipo int.
