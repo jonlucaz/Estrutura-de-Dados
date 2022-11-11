51d788a46bceb3381122b128ff0006b91a52bdb1
jonlucaz <jonathanlucastri@gmail.com>
2022-11-11 09:23:42 -0300

    Modificação simples na pilha

--- pilha/pilha.py
+++ pilha/pilha.py
@@ -1,3 +1,6 @@
+from pythonds.basic.stack import Stack
+
+
 class Stack:
     def __init__(self):
         self.items = []
@@ -11,10 +14,25 @@ class Stack:
     def pop(self):
         return self.items.pop()
 
+    def conteudo(self):
+        return print(self.items)
+
     def peek(self):
-        return self.items[len(self.items)-1]
+        return self.items[len(self.items) - 1]
 
     def size(self):
         return len(self.items)
 
-#página 3.5
+
+# página 3.5
+
+s = Stack()
+print(s.is_empty())
+s.push(5)
+s.push('jon')
+s.push('garrafa')
+
+print(s.is_empty())
+print(s.peek())
+print(s.size())
+s.conteudo()
