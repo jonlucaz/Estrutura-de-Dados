270f343a652643bdd4fc334da0050560152515f7
jonlucaz <jonathanlucastri@gmail.com>
2022-11-19 10:49:18 -0300

    novos arquivos de fila

---
+++ fila/Fila/conceito.py
@@ -0,0 +1,7 @@
+# Fila (Queue)
+# É uma coleção ordenada de itens
+# Segue o modelo FIFO, First In, First Out
+# o fim da fila se chama rear e os novos itens são adicionados nesse rear
+# já o início se chama front e é onde o primeiro item está
+
+
