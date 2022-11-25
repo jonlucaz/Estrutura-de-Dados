8b42fd9774fda96520a20448b0f8719bec2a019b
jonlucaz <jonathanlucastri@gmail.com>
2022-11-25 09:04:34 -0300

    novos arquivos de conceito em fila

---
+++ fila/Fila/elementos.py
@@ -0,0 +1,5 @@
+# Queue() - Cria uma nova fila que está vazia
+# enqueue(item) - insere um novo item no final da fila
+# dequeue() - remove o item no inicio da fila
+# isEmpty() - testa para ver se a fila está vazia
+# size() - retorna o numero de itens na fila
